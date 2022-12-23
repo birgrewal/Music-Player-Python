b995b66262a964b0519c263e51af7d08ef6d480c
birgrewal <bir2006grewal@gmail.com>
2022-12-23 07:17:22 -0900

    Added Add songs option and README.md

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 # Importing Module
 from pygame import mixer
-import customtkinter
+import customtkinter, tkinter
 from PIL import Image
 import eyed3
+import os
 
 mixer.init()
 
 # Variables
-sList = ["Bambiha Bole - Sidhu Moose Wala.mp3", "Old Skool Ft. Sidhu Moose Wala - Prem Dhillon.mp3"]
-current = sList[0]
+sList = []
+musicFolder = "/home/bir/Music/"
+current = ''
 start = False
+fname = "assets/notfound.jpg"
 
 # Functions
 def changeThumb():
-    audio_file = eyed3.load("home/bir/Music/"+current)
-    album_name = audio_file.tag.album
-    artist_name = audio_file.tag.artist
-    for image in audio_file.tag.images:
-        image_file = open("{0} - {1}({2}).jpg".format(artist_name, album_name, image.picture_type), "wb")
-        image_file.write(image.image_data)
-        image_file.close()
+    global fname, playImg, mImg
+    audio_file = eyed3.load(musicFolder+current)
+
+    image = audio_file.tag.images[0]
+    fname = f"assets/song.png"
+    image_file = open(fname, "wb")
+    image_file.write(image.image_data)
+    image_file.close()
+
+    playImg.configure(light_image=Image.open(fname))
+
+    os.remove(fname)
 
 def play():
-    global start
-    i = sList.index(current)
+    global start, current
+    if current == '':
+        current = sList[0]
 
+    print(sList)
+    i = sList.index(current)
     changeThumb()
 
     if start:
         mixer.music.unpause()
     else:
-        mixer.music.load('/home/bir/Music/'+current)
+        mixer.music.load(musicFolder+current)
         mixer.music.play()
         start = True
     
@@ -53,34 +64,89 @@ def next():
     changeThumb()
     start = False
     play()
+
+def prev():
+    global current, start
+    length = len(sList)-1
+    i = sList.index(current)
+
+    if i != length:
+        current = sList[i-1]
+    else:
+        current = sList[0]
     
+    changeThumb()
+    start = False
+    play()
+
+def lsSongs():
+    for f in os.listdir(musicFolder):
+        if '.mp3' in f or '.wav' in f:
+            sList.append(f)
+
+def addsongs():
+    global current, musicFolder,sList
+    songs = list(tkinter.filedialog.askopenfilenames(initialdir=musicFolder,title="Choose a song", filetypes=(("mp3 Files","*.mp3"),)))
+    if len(songs) != 0:
+        sList = songs
+        current = sList[0]
+        musicFolder = ''
+        pause()
+        changeThumb()
+        start = False
+        play()
+
 # Main GUI Code   
 root = customtkinter.CTk()
-root.geometry("840x505")
-root.minsize(840, 505)
-root.maxsize(840, 505)
+root.geometry("840x545")
+root.minsize(840, 545)
+root.maxsize(840, 545)
 root.title("Music Player")
-
-# Variables
 
 # Fonts
 bg = customtkinter.CTkFont(family="Helevetica", size=35, weight = 'bold')
 md = customtkinter.CTkFont(family="Helevetica", size=18, weight='bold')
 sm = customtkinter.CTkFont(family="Helevetica", size=15)
 
+lsSongs()
+
+# Menu Bar
+mainmenu = customtkinter.CTkFrame(root, height=30, width=830)
+mainmenu.grid(row=0, column=0,padx=5, pady=5, sticky="nw")
+
+addSongs = customtkinter.CTkButton(mainmenu, text="Add Songs", width=50, height=20, font=md, corner_radius=0, command=addsongs, fg_color="transparent")
+addSongs.grid(padx=5,ipadx=5,ipady=5,sticky="w")
+
 # Music List
-mList = customtkinter.CTkFrame(master=root, width=300, height=370)
-mList.grid(row=0, column=0, padx=5, pady=5)
+# mList = customtkinter.CTkFrame(master=root, width=300, height=370)
+# mList.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
+
+# textbox = tkinter.Text(mList, highlightthickness=0, bg="black", width=300, height=10)
+# textbox.grid(row=0, column=0, sticky="nsew")
+
+# for i in sList:
+#     item = customtkinter.CTkButton(textbox, text=f"{i[0:25]}...", width=75, height=75, font=md, border_spacing=0, command=play)
+#     item.grid(sticky="w",pady=2)
+
+# scrollbar = customtkinter.CTkScrollbar(textbox, command=textbox.yview)
+# scrollbar.grid(row=0, column=1, sticky="ns")
+
+# connect textbox scroll event to CTk scrollbar
+# textbox.configure(yscrollcommand=scrollbar.set)
 
 # Music Thumbnail
-mThumb = customtkinter.CTkFrame(master=root, width=500, height=370)
-mThumb.grid(row=0, column=1, padx=5, pady=5)
+mThumb = customtkinter.CTkFrame(master=root, width=835, height=370, fg_color="transparent")
+mThumb.grid(row=1, column=0, padx=5, pady=5)
+playImg = customtkinter.CTkImage(light_image=Image.open(fname), size=(835,370))
+mImg = customtkinter.CTkLabel(mThumb, image=playImg, text="")
+mImg.grid(padx=0)
 
 # Music Player Options
-mPlay = customtkinter.CTkFrame(master=root, width=820, height=110, fg_color='transparent')
-mPlay.grid(row=1, columnspan=2, column=0, padx=5, pady=15)
+mPlay = customtkinter.CTkFrame(master=root, width=835, height=110, fg_color='transparent')
+mPlay.grid(row=2, columnspan=2, column=0, padx=5, pady=15)
 
-# playImg = customtkinter.CTkImage(light_image=Image.open("play.png"), dark_image=Image.open("play.png"), size=(95,73))
+nextBtn = customtkinter.CTkButton(mPlay, text="Prev", width=75, height=75, corner_radius=75, font=md, border_spacing=0, command=prev)
+nextBtn.pack(side='left', padx=7)
 
 playBtn = customtkinter.CTkButton(mPlay, text="Play", width=75, height=75, corner_radius=75, font=md, border_spacing=0, command=play)
 playBtn.pack(side='left', padx=7)  
